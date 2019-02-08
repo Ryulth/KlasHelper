cca8d67ae36f06e3f870d42b61a7f9f463904dd1
devOTTO <yoongotto@gmail.com>
2019-02-08 20:50:27 +0900

    Create IOS project

---
+++ frontend/ios/KlasHelper/KlasHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KlasHelper
+//
+//  Created by 융미 on 08/02/2019.
+//  Copyright © 2019 devOTTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
