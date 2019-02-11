c087a8f40fcfffe126a0ce45d290a8cf133f6b36
Ryulth <adboy94@khu.ac.kr>
2019-02-11 12:41:11 +0900

    repo name 변경에 의한 경로 세팅

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIR=/home/uzussregi/project/KlasHelperRemaster
+DIR=/home/uzussregi/project/KlasHelper
 sudo git -C $DIR pull
 docker-compose -f $DIR/docker-compose.yml down
 docker-compose -f $DIR/docker-compose.yml up -d
