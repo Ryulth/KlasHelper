5308988d5d116e7cdd93cfcc828b0e913f278af8
devOTTO <yoongotto@gmail.com>
2019-02-14 13:14:14 +0900

    IOS add view

--- frontend/ios/KlasHelper/KlasHelper/LoginViewController.swift
+++ frontend/ios/KlasHelper/KlasHelper/LoginViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
 
---
+++ frontend/ios/KlasHelper/KlasHelper/MainViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+//    @IBOutlet weak var receivedText: UILabel!
+//    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+//    @IBAction func sendText(_ sender: UIButton) {
+//
+//    }
+}
