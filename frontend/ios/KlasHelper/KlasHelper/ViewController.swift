7a2756184a5f2e81e17d0e8fdf2f890baf193999
devOTTO <yoongotto@gmail.com>
2019-02-21 17:31:39 +0900

    IOS_import Alamofire & test

--- frontend/ios/KlasHelper/KlasHelper/ViewController.swift
+++ frontend/ios/KlasHelper/KlasHelper/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
@@ -23,6 +25,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+ 
 }
 // MARK: UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
@@ -54,5 +57,90 @@ extension ViewController {
         // left view hack to add padding
         passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 3))
         passwordTextField.leftViewMode = .always
+        
+        Alamofire.request(
+            "http://klashelper.ryulth.com/login",
+            method: .post,
+            parameters: ["id":"__", "pw":"__"],
+            encoding: JSONEncoding.default,
+            headers: ["Content-Type":"application/json", "Accept":"application/json" , "appToken":"test"]
+            )
+            .validate(statusCode: 200..<300)
+            .responseJSON {
+                response in
+                if let JSON = response.result.value {
+                    print(JSON)
+              
+                }
+        }
     }
 }
+
+//let MainUrl = "주소"
+//
+//func UploadComplete(PSubURL : String, User_id :String) -> (String, String){
+//
+//    //통신 함수
+//
+//    var SubURL  = PSubURL //서브주소
+//
+//    var Parameter = "?user_id="
+//
+//    var url:NSURL = NSURL(string: self.MainUrl + SubURL + Parameter + User_id)!
+//
+//    var PostValue:NSString = User_id
+//
+//    var post:NSString = "user_id=\(PostValue)"
+//
+//    var PostData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
+//
+//    var postLength:NSString = String(PostData.length)
+//
+//    var request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+//
+//
+//
+//    request.HTTPMethod = "POST"
+//
+//    request.setValue(postLength, forHTTPHeaderField: "Content-Length")
+//
+//    request.setValue("application/x-www-form-rulencoded", forHTTPHeaderField: "Content-Type")
+//
+//    request.setValue("application/json", forHTTPHeaderField: "Accept")
+//
+//
+//
+//    var reponseError : NSError?
+//
+//    var response: NSURLResponse?
+//
+//    var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&reponseError)
+//
+//    let res = response as NSHTTPURLResponse!
+//
+//    if(res.statusCode == 200){
+//
+//        NSLog("Response code : %d", res.statusCode)
+//
+//
+//
+//        var TmpReq = NSString(data: urlData!, encoding: NSUTF8StringEncoding)
+//
+//        var Tmp = split(TmpReq as String, {$0 == ":"}, maxSplit: 1, allowEmptySlices: true)
+//
+//        var TmpHeader = Tmp[0].stringByReplacingPercentEscapesUsingEncoding(NSUTF8StringEncoding)! //디코딩
+//
+//        var TmpContent = Tmp[1].stringByReplacingPercentEscapesUsingEncoding(NSUTF8StringEncoding)! //디코딩
+//
+//        //            NSLog("TemHeader : " + TmpHeader)
+//
+//        //            NSLog("TemContent : " + TmpContent)
+//
+//        return (TmpHeader, TmpContent)
+//
+//    }
+//
+//    return ("Nil", "Nil")
+//
+//}
+
